034533781e51929605ae5b6dae2bf1adb3128111
GeKo1990 <geko1990@gmx.at>
2024-05-09 19:17:09 +0200

    work in progress

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    count_words(text)
+    
+main()
